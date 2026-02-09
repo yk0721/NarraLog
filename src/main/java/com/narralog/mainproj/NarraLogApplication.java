ffd0278e8137dcaf4b1d76a03ce162d1febaa2f7
Kuma Yamashita <kuma.yamashita@choreo.co.jp>
2026-02-09 21:09:24 +0900

    firstcommit

---
+++ src/main/java/com/narralog/mainproj/NarraLogApplication.java
@@ -0,0 +1,13 @@
+package com.narralog.mainproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarraLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarraLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/narralog/mainproj/NarraLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narralog.mainproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarraLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
